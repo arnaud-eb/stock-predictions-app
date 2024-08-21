028cfea1ebf68759fc2b538bd09c580292dbe285
arnaud-eb <arnaud.depierreux@easebest.com>
2024-08-21 06:24:23 +0200

    optimize header image responsiveness

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,8 +7,9 @@ const Header: FC = () => {
       <Image
         src="/logo-dave-text.png"
         alt="Dodgy Dave's Stock Predictions"
+        sizes="(max-width: 400px) 100vw, 50vw"
         width={340}
-        height={340}
+        height={95}
         priority
       />
     </header>
